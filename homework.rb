e1cf897386b3c13f15ab26c2d0cd7857a222a8a5
hudabz <huda_bz@hotmail.com>
2019-05-13 09:27:50 +0300

    Submitting the HW week 4 day 1
    no need to go through the code, nothing works mashallah

---
+++ homework.rb
@@ -0,0 +1,18 @@
+
+
+// # 1
+students.each { |:first_name,:last_name| puts '#{first_name.upcase} #{last_name}'}
+
+// # 2
+
+usersp[0].each order[0]
+
+// # 3
+people.each { if :type = 'COFFEE'
+              count++
+           coffee_total =+ amount
+           avg = coffee_total / count
+           end
+}
+
+
